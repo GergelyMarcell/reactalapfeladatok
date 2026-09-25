16bd426348fbff0df6024d643cee1f8bf0f69c88
Marci1823hun <gergelymarcell1@gmail.com>
2026-09-25 09:36:02 +0200

    formazas

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import Penzvalto from "./pages/Penzvalto.tsx";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./index.css";
 import Kezdolap from "./pages/Kezdolap.tsx";
 import Homerseklet from "./pages/Homerseklet.tsx";
 import BMI from "./pages/BMI.tsx";
 import Szamologep from "./pages/Szamologep.tsx";
+import Penzvalto from "./pages/Penzvalto.tsx";
 import Notfound from "./pages/Notfound.tsx";
 
 createRoot(document.getElementById("root")!).render(
