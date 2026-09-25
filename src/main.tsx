d28df601cb5be902f58f52b8caa4f0cf55ae0b48
Marci1823hun <gergelymarcell1@gmail.com>
2026-09-25 09:34:51 +0200

    router a pagekhez

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,25 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-// import Homerseklet from "./pages/Homerseklet.tsx";
-// import Szamologep from "./pages/Szamologep.tsx";
-// import BMI from "./pages/BMI.tsx";
 import Penzvalto from "./pages/Penzvalto.tsx";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Kezdolap from "./pages/Kezdolap.tsx";
+import Homerseklet from "./pages/Homerseklet.tsx";
+import BMI from "./pages/BMI.tsx";
+import Szamologep from "./pages/Szamologep.tsx";
+import Notfound from "./pages/Notfound.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    {/* <Homerseklet /> */}
-    {/* <Szamologep /> */}
-    {/* <BMI /> */}
-    <Penzvalto />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Kezdolap />} />
+        <Route path="/homerseklet" element={<Homerseklet />} />
+        <Route path="/penzvalto" element={<Penzvalto />} />
+        <Route path="/bmi" element={<BMI />} />
+        <Route path="/szamologep" element={<Szamologep />} />
+        <Route path="*" element={<Notfound />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 );
---
+++ src/pages/Notfound.tsx
@@ -0,0 +1,9 @@
+const Notfound = () => {
+  return (
+    <>
+      <h1>404 - Az oldal nem található</h1>
+    </>
+  );
+};
+
+export default Notfound;
